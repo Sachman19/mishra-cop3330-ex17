cdc1f3a153183290ab921f2fc3791923773dab4e
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 07:07:54 -0400

    Version 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter your weight (in lbs). ");
+        double weight = input.nextDouble();
+
+        System.out.print("Enter 0 if you are male. Enter 1 if female. ");
+        int sex = input.nextInt();
+
+        double ratio;
+        if(sex == 0){
+        ratio = 0.73;
+        }
+        else if(sex == 1){
+        ratio = 0.66;
+        }
+        else{
+            ratio = 0.73;
+            System.out.println("Error: invalid input. Defaulting to 0.");
+        }
+
+        System.out.print("Enter your how much alcohol you've consumed (in oz). ");
+        double alcohol = input.nextDouble();
+
+        System.out.print("Enter the number of hours since your last drink. ");
+        int time = input.nextInt();
+
+
+
+        //BAC = (A × 5.14 / W × r) − .015 × H
+
+        double bac = (alcohol * 5.14 / weight * ratio) - 0.15 * time;
+        System.out.printf("Your BAC is %.2f%n", bac);
+
+        if(bac < 0.08){
+            System.out.print("It is safe for you to drive.");
+        }
+        else{
+            System.out.print("It is not safe for you to drive.");
+        }
 
     }
 }
